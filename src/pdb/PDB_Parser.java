9b8fcc0a60e396a1bd9090427b58f31819db93c0
jellowman <tarfisher@gmail.com>
2016-11-11 22:17:09 -0600

    Fixed PDB Atom Parsing Errors

--- src/pdb/PDB_Parser.java
+++ src/pdb/PDB_Parser.java
@@ -93,18 +93,13 @@ public class PDB_Parser
 			while(nextLine != null)
 			{
 				String[] parts = nextLine.split("\\s+");
-				if(parts[0].equals("HETATM"))
+				
+				if(parts[0].contains("HETATM"))
 				{
+					parts = fixParts(parts);
+					
 					//Check to see if the new atom is part of a new molecule
-					if(parts[3].length() != 1)
-					{
-						String[] fixedParts = parts.clone();
-						parts[4] = fixedParts[3].substring(1, fixedParts[3].length());
-						parts[5] = fixedParts[4];
-						parts[6] = fixedParts[5];
-						parts[7] = fixedParts[6];
-					}
-					if(!molNum.equals(parts[4]))
+					if(!molNum.equals(parts[5]))
 					{
 						String blueprintKey = "";
 						for(Atom atom : tempAtomCollection)
@@ -120,12 +115,13 @@ public class PDB_Parser
 						mol.buildBondsAndAngles();
 						molecules.add(mol);
 						tempAtomCollection = new ArrayList<Atom>();
-						molNum = parts[4];
+						molNum = parts[5];
 					}
 					
 					Atom atom = parseAtom(parts);
 					tempAtomCollection.add(atom);
 				}
+				
 				if(nextLine.equals("END"))
 				{
 					String blueprintKey = "";
@@ -143,6 +139,7 @@ public class PDB_Parser
 					molecules.add(mol);
 					tempAtomCollection = new ArrayList<Atom>();
 				}
+				
 				nextLine = br.readLine();
 			}
 			
@@ -168,16 +165,17 @@ public class PDB_Parser
 		Atom atom = null;
 		String atomType;
 		int atomID;
-		try{
+		//try{
 		atomType = parts[2];
+		System.out.println(parts[1]);
 		atomID = Integer.parseInt(parts[1]);
 		
-		x = Double.parseDouble(parts[5]);
-		y = Double.parseDouble(parts[6]);
-		z = Double.parseDouble(parts[7]);
+		x = Double.parseDouble(parts[parts.length-3]);
+		y = Double.parseDouble(parts[parts.length-2]);
+		z = Double.parseDouble(parts[parts.length-1]);
 		
 		atom = new Atom(atomID, atomType, x, y, z);
-		}catch (NumberFormatException e)
+		/*}catch (NumberFormatException e)
 		{
 			//The "A" and the molecule number may be mashed together
 			if(parts[3].length() != 1)
@@ -185,14 +183,57 @@ public class PDB_Parser
 				atomType = parts[2];
 				atomID = Integer.parseInt(parts[1]);
 				
-				x = Double.parseDouble(parts[4]);
-				y = Double.parseDouble(parts[5]);
-				z = Double.parseDouble(parts[6]);
+				x = Double.parseDouble(parts[parts.length-3]);
+				y = Double.parseDouble(parts[parts.length-2]);
+				z = Double.parseDouble(parts[parts.length-1]);
 				
 				atom = new Atom(atomID, atomType, x, y, z);
 			}
 		}
+		catch (ArrayIndexOutOfBoundsException e)
+		{
+			atomType = parts[2];
+			atomID = Integer.parseInt(parts[1]);
+			
+			x = Double.parseDouble(parts[parts.length-3]);
+			y = Double.parseDouble(parts[parts.length-2]);
+			z = Double.parseDouble(parts[parts.length-1]);
+		}*/
 		return atom;
+	}
+	
+	private String[] fixParts(String[] parts)
+	{
+		//Check to see if the ID was concatenated to the beginning
+		if(!parts[0].equals("HETATM"))
+		{
+			String[] tempParts = new String[parts.length+1];
+			tempParts[0] = "HETATM";
+			tempParts[1] = parts[0].replace("HETATM", "");
+			for(int i = 2; i < tempParts.length; i++)
+			{
+				tempParts[i] = parts[i-1];
+			}
+			parts = tempParts;
+		}
+		
+		//Check to see if the molecule ID and the Atom type are concatenated
+		if(parts[4].length() != 1)
+		{
+			String[] tempParts = new String[parts.length+1];
+			for(int i = 0; i < 4; i++)
+			{
+				tempParts[i] = parts[i];
+			}
+			tempParts[4] = parts[4].substring(0, 1);
+			tempParts[5] = parts[4].substring(1);
+			for(int i = 6; i < tempParts.length; i++)
+			{
+				tempParts[i] = parts[i-1];
+			}
+			parts = tempParts;
+		}
+		return parts;
 	}
 	
 	public ArrayList<Molecule> getMolecules()
